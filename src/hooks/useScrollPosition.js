fb2e413e926058825a896b8d28a238f1d05d86fc
Ivan Palamarchuk <ipz222_pis@student.ztu.edu.ua>
2024-10-18 17:00:52 +0300

    init

---
+++ src/hooks/useScrollPosition.js
@@ -0,0 +1,15 @@
+import { useState, useEffect } from "react";
+
+function useScrollPosition() {
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+  useEffect(() => {
+    const updateScrollPosition = () => setScrollPosition(window.scrollY);
+    window.addEventListener("scroll", updateScrollPosition);
+    return () => window.removeEventListener("scroll", updateScrollPosition);
+  }, []);
+
+  return scrollPosition;
+}
+
+export default useScrollPosition;
