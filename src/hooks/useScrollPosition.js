7cf0eb9b78be94bccc5d8a03ea9e804c8c774f52
Ivan Palamarchuk <ipz222_pis@student.ztu.edu.ua>
2024-10-22 14:43:55 +0300

    refactor: simplify scroll position handling logic

--- src/hooks/useScrollPosition.js
+++ src/hooks/useScrollPosition.js
@@ -1,28 +1,18 @@
 import { useState, useEffect } from "react";
 
 const getPosition = (element) => {
-  if (element == null) return window.scrollY;
-  else return element.scrollTop;
+  return element == window ? window.scrollY : element.scrollTop;
 };
 
 function useScrollPosition(elementRef = null) {
   const [scrollPosition, setScrollPosition] = useState(0);
   useEffect(() => {
-    const element = elementRef?.current;
+    const element = elementRef?.current ?? window;
     const updateScrollPosition = () => setScrollPosition(getPosition(element));
-
-    if (element) {
-      element.addEventListener("scroll", updateScrollPosition);
-    } else {
-      window.addEventListener("scroll", updateScrollPosition);
-    }
+    element.addEventListener("scroll", updateScrollPosition);
 
     return () => {
-      if (element) {
-        element.removeEventListener("scroll", updateScrollPosition);
-      } else {
-        window.removeEventListener("scroll", updateScrollPosition);
-      }
+      element.removeEventListener("scroll", updateScrollPosition);
     };
   }, [elementRef]);
 
